7a59981530a86f098949d0b579c01c1b70294a07
AgentZ <excelsior2018@163.com>
2020-07-07 10:54:20 +0800

    initial commit

---
+++ app/src/main/java/com/itexcelsior/helmet/dialog/loading/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.itexcelsior.helmet.dialog.loading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
